928c95916dfb6312e33d4adc57eb6997d7241b02
Mikhail Melnik <by.zumzoom@gmail.com>
2021-12-28 13:57:18 +0300

    remove redundand tests

--- test/permit.js
+++ test/permit.js
@@ -1,45 +1,10 @@
 const { expect } = require('chai');
 const { web3 } = require('hardhat');
 const { defaultDeadline, EIP712Domain, Permit, DaiLikePermit } = require('../js/permit.js');
-const { trim0x, domainSeparator, buildData, buildDataLikeDai, getPermit, getPermitLikeDai, withTarget } = require('../js/permit.js');
+const { trim0x, buildData, buildDataLikeDai, withTarget } = require('../js/permit.js');
 
 const ERC20PermitMock = artifacts.require('ERC20PermitMock');
 const DaiLikePermitMock = artifacts.require('DaiLikePermitMock');
-
-describe('Constants', async () => {
-    it('should be defaultDeadline', async () => {
-        expect(defaultDeadline).to.be.bignumber.equal('115792089237316195423570985008687907853269984665640564039457584007913129639935');
-    });
-    
-    it('should be EIP712Domain', async () => {
-        expect(EIP712Domain).deep.to.equal([
-            { name: 'name', type: 'string' },
-            { name: 'version', type: 'string' },
-            { name: 'chainId', type: 'uint256' },
-            { name: 'verifyingContract', type: 'address' },
-        ]);
-    });
-
-    it('should be Permit', async () => {
-        expect(Permit).deep.to.equal([
-            { name: 'owner', type: 'address' },
-            { name: 'spender', type: 'address' },
-            { name: 'value', type: 'uint256' },
-            { name: 'nonce', type: 'uint256' },
-            { name: 'deadline', type: 'uint256' },
-        ]);
-    });
-
-    it('should be DaiLikePermit', async () => {
-        expect(DaiLikePermit).deep.to.equal([
-            { name: 'holder', type: 'address' },
-            { name: 'spender', type: 'address' },
-            { name: 'nonce', type: 'uint256' },
-            { name: 'expiry', type: 'uint256' },
-            { name: 'allowed', type: 'bool' },
-        ]);
-    });
-});
 
 describe('Methods', async () => {
     before(async () => {
@@ -62,14 +27,9 @@ describe('Methods', async () => {
         expect(trim0x('123456')).to.be.equal('123456');
     });
 
-    it('should be return domainSeparator', async () => {
-        const ds = domainSeparator(await this.token.name(), '1', this.chainId, this.token.address);
-        expect(ds.length).to.be.equal(66);
-    });
-
-    it('should be return builded data for permit', async () => {
+    it('should correctly build data for permit', async () => {
         const data = buildData(await this.token.name(), '1', this.chainId, this.token.address, this.wallet.address, this.wallet.address, '1', '1');
-        expect(data).deep.to.equal({
+        expect(data).to.be.deep.equal({
             primaryType: 'Permit',
             types: {
                 EIP712Domain: EIP712Domain,
@@ -91,9 +51,9 @@ describe('Methods', async () => {
         });
     });
 
-    it('should be return builded data for permit like dai', async () => {
+    it('should correctly build data for dai-like permit', async () => {
         const data = buildDataLikeDai(await this.daiLikeToken.name(), '1', this.chainId, this.daiLikeToken.address, this.wallet.address, this.wallet.address, '1', true);
-        expect(data).deep.to.equal({
+        expect(data).to.be.deep.equal({
             primaryType: 'Permit',
             types: {
                 EIP712Domain: EIP712Domain,
@@ -115,23 +75,11 @@ describe('Methods', async () => {
         });
     });
 
-    it('should be return permit data', async () => {
-        const permit = await getPermit(this.wallet.address, this.wallet.privateKey, this.token, '1', this.chainId, this.wallet.address, '1');
-        expect(permit.length).to.be.equal(450);
-        // extend test see in Permitable.test.js - should be permitted for IERC20Permit
+    it('should concat target with prefixed data', async () => {
+        expect(withTarget('0x123456', '0x123456')).to.be.equal('0x123456123456');
     });
 
-    it('should be return permit like dai data', async () => {
-        const permit = await getPermitLikeDai(this.wallet.address, this.wallet.privateKey, this.daiLikeToken, '1', this.chainId, this.wallet.address, true);
-        expect(permit.length).to.be.equal(514);
-        // extend test see in Permitable.test.js - should be permitted for IDaiLikePermit
-    });
-
-    it('should be join trimmed data to target', async () => {
-        expect(withTarget('123456', '0x123456')).to.be.equal('123456123456');
-    });
-
-    it('should be join data to target', async () => {
-        expect(withTarget('123456', '123456')).to.be.equal('123456123456');
+    it('should concat target with raw data', async () => {
+        expect(withTarget('0x123456', '123456')).to.be.equal('0x123456123456');
     });
 });
