c13875b29f1801d86192fceb251878de75dcca7e
Gleb Alekseev <alekseev.gleb@gmail.com>
2022-03-18 12:44:40 +0300

    Bugfix - two or more contracts in file
    Fixed uncaught exception if there are two or more contracts located in 1 file

--- utils/solidity-docgen-helpers.js
+++ utils/solidity-docgen-helpers.js
@@ -7,18 +7,25 @@ module.exports = {
     },
     getRelativeDocPath (contractName, contractsDir, sources) {
         function getRelativePath (contractPath) {
-            if (contractPath.startsWith(contractsDir)) {
-                return contractPath.substr(contractsDir.length + 1).replace('.sol', '.md');
-            }
-            if (contractPath.startsWith('@openzeppelin')) {
-                const regexMatch = contractPath.match(/@openzeppelin\/contracts\/(.+)\/([^/]+)\.sol/);
-                return `https://docs.openzeppelin.com/contracts/3.x/api/${regexMatch[1]}#${regexMatch[2]}`;
+            if (contractPath){
+                if (contractPath.startsWith(contractsDir)) {
+                    return contractPath.substr(contractsDir.length + 1).replace('.sol', '.md');
+                }
+                if (contractPath.startsWith('@openzeppelin')) {
+                    const regexMatch = contractPath.match(/@openzeppelin\/contracts\/(.+)\/([^/]+)\.sol/);
+                    return `https://docs.openzeppelin.com/contracts/3.x/api/${regexMatch[1]}#${regexMatch[2]}`;
+                }
             }
             return null;
         }
 
         const sourcesKeys = Object.keys(sources);
         const contractPath = sourcesKeys.find(x => x.includes(contractName));
+
+        if (!contractPath){
+            console.log('WARNING: file ' + contractName + '.sol not found in source files.');
+        }
+        
         return getRelativePath(contractPath);
     },
 };
