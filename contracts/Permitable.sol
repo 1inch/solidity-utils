5037482d58175af9d6acf081763d3d753c02f6b5
Mikhail Melnik <by.zumzoom@gmail.com>
2022-04-12 18:23:11 +0400

    simplify Permitable

--- contracts/Permitable.sol
+++ contracts/Permitable.sol
@@ -13,13 +13,12 @@ contract Permitable {
     function _permit(address token, bytes calldata permit) internal virtual {
         if (permit.length > 0) {
             bool success;
-            bytes memory result;
             if (permit.length == 32 * 7) {
                 // solhint-disable-next-line avoid-low-level-calls
-                (success, result) = token.call(abi.encodePacked(IERC20Permit.permit.selector, permit));
+                (success,) = token.call(abi.encodePacked(IERC20Permit.permit.selector, permit));
             } else if (permit.length == 32 * 8) {
                 // solhint-disable-next-line avoid-low-level-calls
-                (success, result) = token.call(abi.encodePacked(IDaiLikePermit.permit.selector, permit));
+                (success,) = token.call(abi.encodePacked(IDaiLikePermit.permit.selector, permit));
             } else {
                 revert BadPermitLength();
             }
