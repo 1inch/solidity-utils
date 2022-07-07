eec6b523860af5215a8dd196fe3aff3a4d252fc9
Mikhail Melnik <by.zumzoom@gmail.com>
2022-07-07 20:22:18 +0800

    use IWETH

--- contracts/OnlyWethReceiver.sol
+++ contracts/OnlyWethReceiver.sol
@@ -3,18 +3,19 @@
 pragma solidity ^0.8.0;
 pragma abicoder v1;
 
+import "./interfaces/IWETH.sol";
+
 abstract contract OnlyWethReceiver {
     error EthDepositRejected();
 
-    // solhint-disable-next-line var-name-mixedcase
-    address private immutable _WETH;
+    IWETH internal immutable _WETH;  // solhint-disable-line var-name-mixedcase
 
-    constructor(address weth) {
+    constructor(IWETH weth) {
         _WETH = weth;
     }
 
     receive() external payable {
         // solhint-disable-next-line avoid-tx-origin
-        if (msg.sender != _WETH) revert EthDepositRejected();
+        if (msg.sender != address(_WETH)) revert EthDepositRejected();
     }
 }
