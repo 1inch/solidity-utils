f7d7f0ef43eb5ee52ccb6130d99428569de20ffa
Mikhail Melnik <by.zumzoom@gmail.com>
2022-10-07 13:43:50 +0400

    refactor a bit

--- test/contracts/SafestERC20.test.ts
+++ test/contracts/SafestERC20.test.ts
@@ -2,7 +2,7 @@ import { constants, expect } from '../../src/prelude';
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
 import { loadFixture } from '@nomicfoundation/hardhat-network-helpers';
 import { ethers } from 'hardhat';
-import { ContractFactory } from 'ethers';
+import { Contract, ContractFactory } from 'ethers';
 
 const Permit = [
     { name: 'owner', type: 'address' },
@@ -25,7 +25,7 @@ describe('SafeERC20', async () => {
     const deployWrapperSimple = async () => {
         const wrapper = await SafeERC20Wrapper.deploy(spender.address);
         await wrapper.deployed();
-        return wrapper;
+        return { wrapper };
     };
 
     const deployWrapperFalseMock = async () => {
@@ -34,7 +34,7 @@ describe('SafeERC20', async () => {
         await falseMock.deployed();
         const wrapper = await SafeERC20Wrapper.deploy(falseMock.address);
         await wrapper.deployed();
-        return wrapper;
+        return { wrapper };
     };
 
     const deployWrapperTrueMock = async () => {
@@ -43,7 +43,7 @@ describe('SafeERC20', async () => {
         await trueMock.deployed();
         const wrapper = await SafeERC20Wrapper.deploy(trueMock.address);
         await wrapper.deployed();
-        return wrapper;
+        return { wrapper };
     };
 
     const deployWrapperNoReturnMock = async () => {
@@ -52,7 +52,7 @@ describe('SafeERC20', async () => {
         await noReturnMock.deployed();
         const wrapper = await SafeERC20Wrapper.deploy(noReturnMock.address);
         await wrapper.deployed();
-        return wrapper;
+        return { wrapper };
     };
 
     const deployWrapperZeroApprove = async () => {
@@ -61,7 +61,7 @@ describe('SafeERC20', async () => {
         await zeroApprove.deployed();
         const wrapper = await SafeERC20Wrapper.deploy(zeroApprove.address);
         await wrapper.deployed();
-        return wrapper;
+        return { wrapper };
     };
 
     const deployPermitNoRevertAndSign = async () => {
@@ -92,35 +92,35 @@ describe('SafeERC20', async () => {
     };
 
     describe('with address that has no contract code', async () => {
-        shouldRevertOnAllCalls(['SafeTransferFailed', 'SafeTransferFromFailed', 'ForceApproveFailed', ''], false);
+        shouldRevertOnAllCalls(['SafeTransferFailed', 'SafeTransferFromFailed', 'ForceApproveFailed', ''], deployWrapperSimple);
     });
 
     describe('with token that returns false on all calls', async () => {
-        shouldRevertOnAllCalls(['SafeTransferFailed', 'SafeTransferFromFailed', 'ForceApproveFailed'], true);
+        shouldRevertOnAllCalls(['SafeTransferFailed', 'SafeTransferFromFailed', 'ForceApproveFailed'], deployWrapperFalseMock);
     });
 
     describe('with token that returns true on all calls', async () => {
-        shouldOnlyRevertOnErrors(true);
+        shouldOnlyRevertOnErrors(deployWrapperTrueMock);
     });
 
     describe('with token that returns no boolean values', async () => {
-        shouldOnlyRevertOnErrors(false);
+        shouldOnlyRevertOnErrors(deployWrapperNoReturnMock);
     });
 
     describe('non-zero to non-zero approval forbidden', async () => {
         it('zero to non-zero approval should pass', async () => {
-            const wrapper = await loadFixture(deployWrapperZeroApprove);
+            const { wrapper}  = await loadFixture(deployWrapperZeroApprove);
             await wrapper.approve(100);
         });
 
         it('non-zero to non-zero approval should pass', async () => {
-            const wrapper = await loadFixture(deployWrapperZeroApprove);
+            const { wrapper}  = await loadFixture(deployWrapperZeroApprove);
             await wrapper.approve(100);
             await wrapper.approve(100);
         });
 
         it('non-zero to zero to non-zero approval should pass', async () => {
-            const wrapper = await loadFixture(deployWrapperZeroApprove);
+            const { wrapper } = await loadFixture(deployWrapperZeroApprove);
             await wrapper.approve(100);
             await wrapper.approve(0);
             await wrapper.approve(100);
@@ -218,24 +218,24 @@ describe('SafeERC20', async () => {
         });
     });
 
-    function shouldRevertOnAllCalls(reasons: string[], isFalseMock: boolean) {
+    function shouldRevertOnAllCalls(reasons: string[], fixture: () => Promise<{wrapper: Contract;}>) {
         it('reverts on transfer', async () => {
-            const wrapper = await loadFixture(isFalseMock ? deployWrapperFalseMock : deployWrapperSimple);
+            const { wrapper } = await loadFixture(fixture);
             await expect(wrapper.transfer()).to.be.revertedWithCustomError(wrapper, reasons[0]);
         });
 
         it('reverts on transferFrom', async () => {
-            const wrapper = await loadFixture(isFalseMock ? deployWrapperFalseMock : deployWrapperSimple);
+            const { wrapper } = await loadFixture(fixture);
             await expect(wrapper.transferFrom()).to.be.revertedWithCustomError(wrapper, reasons[1]);
         });
 
         it('reverts on approve', async () => {
-            const wrapper = await loadFixture(isFalseMock ? deployWrapperFalseMock : deployWrapperSimple);
+            const { wrapper } = await loadFixture(fixture);
             await expect(wrapper.approve(0)).to.be.revertedWithCustomError(wrapper, reasons[2]);
         });
 
         it('reverts on increaseAllowance', async () => {
-            const wrapper = await loadFixture(isFalseMock ? deployWrapperFalseMock : deployWrapperSimple);
+            const { wrapper } = await loadFixture(fixture);
             if (reasons.length === 3) {
                 await expect(wrapper.increaseAllowance(0)).to.be.revertedWithCustomError(wrapper, reasons[2]);
             } else {
@@ -244,7 +244,7 @@ describe('SafeERC20', async () => {
         });
 
         it('reverts on decreaseAllowance', async () => {
-            const wrapper = await loadFixture(isFalseMock ? deployWrapperFalseMock : deployWrapperSimple);
+            const { wrapper } = await loadFixture(fixture);
             if (reasons.length === 3) {
                 await expect(wrapper.decreaseAllowance(0)).to.be.revertedWithCustomError(wrapper, reasons[2]);
             } else {
@@ -253,36 +253,36 @@ describe('SafeERC20', async () => {
         });
     }
 
-    function shouldOnlyRevertOnErrors(isTrueMock: boolean) {
+    function shouldOnlyRevertOnErrors(fixture: () => Promise<{wrapper: Contract;}>) {
         it("doesn't revert on transfer", async () => {
-            const wrapper = await loadFixture(isTrueMock ? deployWrapperTrueMock : deployWrapperNoReturnMock);
+            const { wrapper } = await loadFixture(fixture);
             await wrapper.transfer();
         });
 
         it("doesn't revert on transferFrom", async () => {
-            const wrapper = await loadFixture(isTrueMock ? deployWrapperTrueMock : deployWrapperNoReturnMock);
+            const { wrapper } = await loadFixture(fixture);
             await wrapper.transferFrom();
         });
 
         describe('approvals', function () {
             describe('with zero allowance', async () => {
                 it("doesn't revert when approving a non-zero allowance", async () => {
-                    const wrapper = await loadFixture(isTrueMock ? deployWrapperTrueMock : deployWrapperNoReturnMock);
+                    const { wrapper } = await loadFixture(fixture);
                     await wrapper.approve(100);
                 });
 
                 it("doesn't revert when approving a zero allowance", async () => {
-                    const wrapper = await loadFixture(isTrueMock ? deployWrapperTrueMock : deployWrapperNoReturnMock);
+                    const { wrapper } = await loadFixture(fixture);
                     await wrapper.approve(0);
                 });
 
                 it("doesn't revert when increasing the allowance", async () => {
-                    const wrapper = await loadFixture(isTrueMock ? deployWrapperTrueMock : deployWrapperNoReturnMock);
+                    const { wrapper } = await loadFixture(fixture);
                     await wrapper.increaseAllowance(10);
                 });
 
                 it('reverts when decreasing the allowance', async () => {
-                    const wrapper = await loadFixture(isTrueMock ? deployWrapperTrueMock : deployWrapperNoReturnMock);
+                    const { wrapper } = await loadFixture(fixture);
                     await expect(wrapper.decreaseAllowance(10)).to.be.revertedWithCustomError(
                         wrapper,
                         'SafeDecreaseAllowanceFailed',
@@ -292,31 +292,31 @@ describe('SafeERC20', async () => {
 
             describe('with non-zero allowance', async () => {
                 it("doesn't revert when approving a non-zero allowance", async () => {
-                    const wrapper = await loadFixture(isTrueMock ? deployWrapperTrueMock : deployWrapperNoReturnMock);
+                    const { wrapper } = await loadFixture(fixture);
                     await wrapper.setAllowance(100);
                     await wrapper.approve(20);
                 });
 
                 it("doesn't revert when approving a zero allowance", async () => {
-                    const wrapper = await loadFixture(isTrueMock ? deployWrapperTrueMock : deployWrapperNoReturnMock);
+                    const { wrapper } = await loadFixture(fixture);
                     await wrapper.setAllowance(100);
                     await wrapper.approve(0);
                 });
 
                 it("doesn't revert when increasing the allowance", async () => {
-                    const wrapper = await loadFixture(isTrueMock ? deployWrapperTrueMock : deployWrapperNoReturnMock);
+                    const { wrapper } = await loadFixture(fixture);
                     await wrapper.setAllowance(100);
                     await wrapper.increaseAllowance(10);
                 });
 
                 it("doesn't revert when decreasing the allowance to a positive value", async () => {
-                    const wrapper = await loadFixture(isTrueMock ? deployWrapperTrueMock : deployWrapperNoReturnMock);
+                    const { wrapper } = await loadFixture(fixture);
                     await wrapper.setAllowance(100);
                     await wrapper.decreaseAllowance(50);
                 });
 
                 it('reverts when decreasing the allowance to a negative value', async () => {
-                    const wrapper = await loadFixture(isTrueMock ? deployWrapperTrueMock : deployWrapperNoReturnMock);
+                    const { wrapper } = await loadFixture(fixture);
                     await wrapper.setAllowance(100);
                     await expect(wrapper.decreaseAllowance(200)).to.be.revertedWithCustomError(
                         wrapper,
