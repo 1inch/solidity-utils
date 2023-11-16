ed02a61d4b37e3db2bce88c63720f7c5bc073a81
Denis <zzomrot.ne@gmail.com>
2023-11-16 15:03:43 +0400

    Separate module for hardhat-setup methods

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -9,7 +9,7 @@ require('solidity-coverage'); // require because no TS typings available
 import dotenv from 'dotenv';
 import { HardhatUserConfig } from 'hardhat/config';
 import { HardhatNetworkUserConfig } from 'hardhat/types';
-import { Networks, getNetwork } from './src/networks';
+import { Networks, getNetwork } from './src/hardhat-setup/networks';
 
 dotenv.config();
 
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 // created from 'create-ts-index'
 
+export * from './hardhat-setup';
 export * from './asserts';
-export * from './networks';
 export * from './permit';
 export * from './prelude';
 export * from './profileEVM';
