c38204bd3f282684890b28204adf4ecd73256ae4
Denis <zzomrot.ne@gmail.com>
2024-02-28 11:59:44 +0000

    Patch PermitAndCall natspecs

--- contracts/PermitAndCall.sol
+++ contracts/PermitAndCall.sol
@@ -8,14 +8,15 @@ import { SafeERC20 } from "./libraries/SafeERC20.sol";
 /**
  * @title PermitAndCall
  * @dev Abstract contract to support permit and action execution in a single transaction.
- * Allows tokens that implement EIP-2612 permits to be approved and spent in a single transaction.
+ * Allows tokens that implement EIP-2612 permits, DAI-like permits and Permit2 to be approved and spent in a single transaction.
  */
 abstract contract PermitAndCall {
     using SafeERC20 for IERC20;
 
     /**
      * @notice Executes a permit for an ERC20 token and then a specified action in a single transaction.
-     * @param permit The permit data for the ERC20 token, allowing this contract to spend the token.
+     * @param permit ERC20 token address (20 bytes) concatinated with the permit data, allowing this contract to spend the token.
+     * Format: [token address (20 bytes)][permit data]
      * @param action The data representing the action to be executed after the permit.
      */
     function permitAndCall(bytes calldata permit, bytes calldata action) external payable {
