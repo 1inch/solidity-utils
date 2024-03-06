0de4c7192e6850eaf822a1cf83b71447cab6f6fe
Anton Bukov <k06aaa@gmail.com>
2024-03-06 01:42:27 +0100

    Move mixins into mixins subfolder

--- contracts/mixins/PermitAndCall.sol
+++ contracts/mixins/PermitAndCall.sol
@@ -3,7 +3,7 @@
 pragma solidity ^0.8.0;
 
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import { SafeERC20 } from "./libraries/SafeERC20.sol";
+import { SafeERC20 } from "../libraries/SafeERC20.sol";
 
 abstract contract PermitAndCall {
     using SafeERC20 for IERC20;
