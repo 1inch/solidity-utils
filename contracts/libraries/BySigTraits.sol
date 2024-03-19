ff8becbeaa01b171614c983452c204c898c4344a
Mikhail Melnik <by.zumzoom@gmail.com>
2024-03-19 22:09:37 +0400

    add missing constant and make them private

--- contracts/libraries/BySigTraits.sol
+++ contracts/libraries/BySigTraits.sol
@@ -17,28 +17,29 @@ library BySigTraits {
         Unique
     }
 
-    uint256 constant public DEADLINE_BIT_SHIFT = 208;
-    uint256 constant public DEADLINE_BIT_MASK = (1 << 40) - 1;
-    uint256 constant public RELAYER_BIT_SHIFT = 128;
-    uint256 constant public RELAYER_BIT_MASK = (1 << 80) - 1;
-    uint256 constant public NONCE_MASK = (1 << 128) - 1;
+    uint256 constant private _TYPE_BIT_SHIFT = 254;
+    uint256 constant private _DEADLINE_BIT_SHIFT = 208;
+    uint256 constant private _DEADLINE_BIT_MASK = (1 << 40) - 1;
+    uint256 constant private _RELAYER_BIT_SHIFT = 128;
+    uint256 constant private _RELAYER_BIT_MASK = (1 << 80) - 1;
+    uint256 constant private _NONCE_MASK = (1 << 128) - 1;
 
     function nonceType(Value traits) internal pure returns(NonceType) {
-        uint256 bits = Value.unwrap(traits) >> 254;
-        if (bits > uint256(NonceType.Unique)) revert WrongNonceType();
-        return NonceType(bits);
+        uint256 _type = Value.unwrap(traits) >> _TYPE_BIT_SHIFT;
+        if (_type > uint256(NonceType.Unique)) revert WrongNonceType();
+        return NonceType(_type);
     }
 
     function deadline(Value traits) internal pure returns(uint256) {
-        return (Value.unwrap(traits) >> DEADLINE_BIT_SHIFT) & DEADLINE_BIT_MASK;
+        return (Value.unwrap(traits) >> _DEADLINE_BIT_SHIFT) & _DEADLINE_BIT_MASK;
     }
 
     function isRelayerAllowed(Value traits, address relayer) internal pure returns(bool) {
-        uint256 lowerBits = (Value.unwrap(traits) >> RELAYER_BIT_SHIFT) & RELAYER_BIT_MASK;
-        return lowerBits == 0 || (uint160(relayer) & RELAYER_BIT_MASK) == lowerBits;
+        uint256 lowerBits = (Value.unwrap(traits) >> _RELAYER_BIT_SHIFT) & _RELAYER_BIT_MASK;
+        return lowerBits == 0 || (uint160(relayer) & _RELAYER_BIT_MASK) == lowerBits;
     }
 
     function nonce(Value traits) internal pure returns(uint256) {
-        return Value.unwrap(traits) & NONCE_MASK;
+        return Value.unwrap(traits) & _NONCE_MASK;
     }
 }
