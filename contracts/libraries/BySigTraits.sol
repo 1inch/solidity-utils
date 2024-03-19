33bcaccc8fc94f82e077ef3c14ae0da485689775
Mikhail Melnik <by.zumzoom@gmail.com>
2024-03-19 22:15:52 +0400

    keep constants public for reusability

--- contracts/libraries/BySigTraits.sol
+++ contracts/libraries/BySigTraits.sol
@@ -18,29 +18,29 @@ library BySigTraits {
         Unique
     }
 
-    uint256 constant private _TYPE_BIT_SHIFT = 254;
-    uint256 constant private _DEADLINE_BIT_SHIFT = 208;
-    uint256 constant private _DEADLINE_BIT_MASK = (1 << 40) - 1;
-    uint256 constant private _RELAYER_BIT_SHIFT = 128;
-    uint256 constant private _RELAYER_BIT_MASK = (1 << 80) - 1;
-    uint256 constant private _NONCE_MASK = (1 << 128) - 1;
+    uint256 constant public TYPE_BIT_SHIFT = 254;
+    uint256 constant public DEADLINE_BIT_SHIFT = 208;
+    uint256 constant public DEADLINE_BIT_MASK = (1 << 40) - 1;
+    uint256 constant public RELAYER_BIT_SHIFT = 128;
+    uint256 constant public RELAYER_BIT_MASK = (1 << 80) - 1;
+    uint256 constant public NONCE_MASK = (1 << 128) - 1;
 
     function nonceType(Value traits) internal pure returns(NonceType) {
-        uint256 _type = Value.unwrap(traits) >> _TYPE_BIT_SHIFT;
+        uint256 _type = Value.unwrap(traits) >> TYPE_BIT_SHIFT;
         if (_type > uint256(NonceType.Unique)) revert WrongNonceType();
         return NonceType(_type);
     }
 
     function deadline(Value traits) internal pure returns(uint256) {
-        return (Value.unwrap(traits) >> _DEADLINE_BIT_SHIFT) & _DEADLINE_BIT_MASK;
+        return (Value.unwrap(traits) >> DEADLINE_BIT_SHIFT) & DEADLINE_BIT_MASK;
     }
 
     function isRelayerAllowed(Value traits, address relayer) internal pure returns(bool) {
-        uint256 relayerBits = (Value.unwrap(traits) >> _RELAYER_BIT_SHIFT) & _RELAYER_BIT_MASK;
-        return relayerBits == 0 || (uint160(relayer) & _RELAYER_BIT_MASK) == relayerBits;
+        uint256 relayerBits = (Value.unwrap(traits) >> RELAYER_BIT_SHIFT) & RELAYER_BIT_MASK;
+        return relayerBits == 0 || (uint160(relayer) & RELAYER_BIT_MASK) == relayerBits;
     }
 
     function nonce(Value traits) internal pure returns(uint256) {
-        return Value.unwrap(traits) & _NONCE_MASK;
+        return Value.unwrap(traits) & NONCE_MASK;
     }
 }
