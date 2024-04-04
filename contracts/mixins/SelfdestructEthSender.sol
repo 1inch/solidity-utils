588be9345d2d61091404c85d1f44534354489276
Denis <zzomrot.ne@gmail.com>
2024-04-04 02:01:12 +0100

    Add natspecs for SelfdestructEthSender and mock

--- contracts/mixins/SelfdestructEthSender.sol
+++ contracts/mixins/SelfdestructEthSender.sol
@@ -2,8 +2,17 @@
 
 pragma solidity ^0.8.25;
 
-
+/**
+ * @title SelfdestructEthSender
+ * @notice A one-time-use contract for cost-effective ETH transfers using the `selfdestruct` mechanism.
+ * @dev Upon construction, this contract verifies EVM compatibility with the Cancun upgrade, specifically
+ * testing for support of transient storage via `tload`. It is intended for single-use, allowing for ETH to
+ * be sent more cheaply by self-destructing and sending its balance to a designated address.
+ */
 abstract contract SelfdestructEthSender {
+    /**
+     * @dev Initializes the contract, verifying compatibility with the Cancun EVM upgrade through transient storage support check.
+     */
     constructor() {
         // tload is done to verify that the EVM is cancun-compatible
         // solhint-disable-next-line no-inline-assembly
@@ -12,6 +21,10 @@ abstract contract SelfdestructEthSender {
         }
     }
 
+    /**
+     * @notice Self-destruct contract, transferring the entire ETH balance of the contract to the specified address.
+     * @param receiver The recipient address of the contract's ETH balance.
+     */
     function stopAndTransferBalance(address payable receiver) external {
         selfdestruct(receiver);
     }
