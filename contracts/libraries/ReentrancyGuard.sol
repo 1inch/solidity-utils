b54387faa2eedba357db626e587a1db4d5e33514
Anton Bukov <k06aaa@gmail.com>
2025-07-26 02:25:21 +0200

    Rename ReentrancyGuard into TransientLock and ReentrancyGuradBase into ReentrancyGuard

--- contracts/libraries/ReentrancyGuard.sol
+++ contracts/libraries/ReentrancyGuard.sol
@@ -2,27 +2,53 @@
 
 pragma solidity ^0.8.0;
 
-import { TransientLib, tuint256 } from "./Transient.sol";
+import { TransientLock, TransientLockLib } from "./TransientLock.sol";
 
-struct ReentrancyGuard {
-    tuint256 _raw;
-}
+/// @dev Base contract with reentrancy guard functionality using transient storage locks.
+///
+/// Use private _lock defined in this contract:
+/// ```solidity
+/// function swap(...) external nonReentrant {
+/// function doMagic(...) external onlyNonReentrantCall {
+/// ```
+///
+/// Or use your own locks for more granular control:
+/// ```solidity
+/// TransientLock private _myLock;
+/// function swap(...) external nonReentrantLock(_myLock) {
+/// function doMagic(...) external onlyNonReentrantCallLock(_myLock) {
+/// ```
+///
+abstract contract ReentrancyGuard {
+    using TransientLockLib for TransientLock;
 
-library ReentrancyGuardLib {
-    using TransientLib for tuint256;
+    error MissingNonReentrantModifier();
 
-    error ReentrantCallDetected();
-    error EnterLeaveDisbalance();
+    TransientLock private _lock;
 
-    function enter(ReentrancyGuard storage self) internal {
-        if (self._raw.inc() != 1) revert ReentrantCallDetected();
+    modifier nonReentrant {
+        _lock.lock();
+        _;
+        _lock.unlock();
     }
 
-    function enterNoIncrement(ReentrancyGuard storage self) internal view {
-        if (self._raw.tload() != 0) revert ReentrantCallDetected();
+    modifier onlyNonReentrantCall {
+        if (!_inNonReentrantCall()) revert MissingNonReentrantModifier();
+        _;
     }
 
-    function leave(ReentrancyGuard storage self) internal {
-        self._raw.dec(EnterLeaveDisbalance.selector);
+    modifier nonReentrantLock(TransientLock storage lock) {
+        lock.lock();
+        _;
+        lock.unlock();
+    }
+
+    modifier onlyNonReentrantCallLock(TransientLock storage lock) {
+        if (!lock.isLocked()) revert MissingNonReentrantModifier();
+        _;
+    }
+
+    function _inNonReentrantCall() internal view returns (bool) {
+        return _lock.isLocked();
     }
 }
