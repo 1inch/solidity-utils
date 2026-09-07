2a386239e8fcb128adff44c890f021c38e60393d
Stepan Chekhovskoi <chestedos@gmail.com>
2026-09-07 13:40:56 +0000

    relax ^0.8.30

--- contracts/libraries/Calldata.sol
+++ contracts/libraries/Calldata.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.30;
+
+pragma solidity ^0.8.27;
 
 /**
  * @title Calldata
--- contracts/libraries/TransientLockUnsafe.sol
+++ contracts/libraries/TransientLockUnsafe.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.30;
+
+pragma solidity ^0.8.27;
 
 import { TransientUnsafe, tuint256 } from "./TransientUnsafe.sol";
 import { TransientLock } from "./TransientLock.sol";
